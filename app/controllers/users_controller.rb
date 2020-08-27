78ded84900f6289f47590c49cfc1bf602b953781
ZaneN8 <zanelnewsom@gmail.com>
2020-08-27 16:38:20 -0600

    Controlers for doctor and user running

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :new, :destroy]
+
   def index
+    @users = User.all
   end
 
   def show
+
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(params[:first_name, :last_name])
   end
 end
